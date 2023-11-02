9ca2979bc13eee1e3e27978a254d75e4f649f0bc
AbhiParmar01 <149404529+AbhiParmar01@users.noreply.github.com>
2023-11-02 21:11:22 +0530

    Add files via upload

---
+++ basics.cpp
@@ -0,0 +1,87 @@
+//*******************************IF CONDITION***********************************
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//	if(20>18){
+//		cout<<"Abhi";
+//	}
+//	return 0;
+//}
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//	int t = 20;
+//	if(t<18){
+//		cout<<"Good";
+//	}
+//	else{
+//		cout<<"Ahhh";
+//	}
+//	return 0;
+//}
+//#include<bits/stdc++.h>
+//using namespace std;
+//int mani(){
+//	int r= 22;
+//	if(r>16){
+//		cout<<"Abhi";
+//	}
+//	else if(r<17){
+//		cout<<"OM";
+//	}
+//	else{
+//		cout<<"Kuch bhi";
+//	}
+//	return 0;
+//}
+//*******************************Loop*************************
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//	int i = 0;
+//	while(i<5){
+//		cout<<i<<endl;
+//			i++;
+//	}
+//	return 0;
+//}
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//	int i= 0;
+//	do{
+//		cout<<i<<endl;
+//		i++;
+//	}
+//	while(i<8);
+//	return 0;
+//}
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//	for(int i=0;i<10;i++){
+//		cout<<i<<endl;
+//	}
+//	return 0;
+//}
+//**************Array*****************
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//	string cars [5]= {"BMW","FARARE","TATA","LAMBORGINE","VOLVO"};
+//	cout<<cars[0,2];
+//	return 0;
+//}
+#include<bits/stdc++.h>
+using namespace std;
+void laptop(){
+	for(int i = 0;i<10;i++){
+		cout<<i<<endl;
+	}
+}
+int main(){
+		string cars[3]={"BMW","Toyta","Tata"};
+	cout<<cars[2];
+	laptop();
+	return 0;
+}
